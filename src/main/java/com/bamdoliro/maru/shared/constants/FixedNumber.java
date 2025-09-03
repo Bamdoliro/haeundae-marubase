6350b4def7902cd3f42b43115e5f24b53ccbe941
hsm <hll2071@naver.com>
2025-09-03 23:53:46 +0900

    refactor(#7) : 정원의 1.5배수 추가로 수정
    정원의 1.5배수를 뽑아야 되는데 1.3배수로 되어있어 수정했습니다.

--- src/main/java/com/bamdoliro/maru/shared/constants/FixedNumber.java
+++ src/main/java/com/bamdoliro/maru/shared/constants/FixedNumber.java
@@ -10,6 +10,6 @@ public class FixedNumber {
     public static final int SPECIAL = 36;
     public static final int SPECIAL_ADMISSION = 3;
     public static final int NATIONAL_VETERANS_EDUCATION = 5;
-    public static final double MULTIPLE = 1.3;
+    public static final double MULTIPLE = 1.5;
     public static final double OTHER_REGION_RATE = 0.5;
 }
