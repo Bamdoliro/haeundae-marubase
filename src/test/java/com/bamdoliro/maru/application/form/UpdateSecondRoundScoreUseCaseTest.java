cf64c5dee2af40820a18989f83b99b43f7c97acd
hsm <hll2071@naver.com>
2025-09-06 02:49:38 +0900

    refactor(#12) : 불필요한 import문 삭제
    불필요한 import문을 삭제했습니다.

--- src/test/java/com/bamdoliro/maru/application/form/UpdateSecondRoundScoreUseCaseTest.java
+++ src/test/java/com/bamdoliro/maru/application/form/UpdateSecondRoundScoreUseCaseTest.java
@@ -27,7 +27,6 @@ import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ActiveProfiles("test")
 @ExtendWith(DatabaseClearExtension.class)
