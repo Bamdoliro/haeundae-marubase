caa52e973e46cbb02c9473a59b914e5bfd8e40f3
hsm <hll2071@naver.com>
2025-10-21 23:27:05 +0900

    refactor(#77) : 타지역 50% 제한 제거
    타지역 합격자가 50%를 넘지 못하는 제한을 제거했습니다.

--- src/main/java/com/bamdoliro/maru/application/form/SelectFirstPassUseCase.java
+++ src/main/java/com/bamdoliro/maru/application/form/SelectFirstPassUseCase.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 @RequiredArgsConstructor
@@ -19,7 +18,6 @@ public class SelectFirstPassUseCase {
 
     private final FormRepository formRepository;
     private final CalculateFormScoreService calculateFormScoreService;
-    private final AtomicInteger otherRegionCount = new AtomicInteger((int) (calculateMultiple(FixedNumber.TOTAL) * FixedNumber.OTHER_REGION_RATE));
 
     @Transactional
     public void execute() {
@@ -70,16 +68,8 @@ public class SelectFirstPassUseCase {
 
         for (Form form : formList) {
             if (count > 0) {
-                if (form.getEducation().getSchool().isBusan()) {
-                    form.firstPass();
-                    count--;
-                } else if (otherRegionCount.intValue() > 0) {
-                    form.firstPass();
-                    otherRegionCount.getAndDecrement();
-                    count--;
-                } else {
-                    action.accept(form);
-                }
+                form.firstPass();
+                count--;
             } else if (form.getScore().getFirstRoundScore().equals(lastScore)) {
                 form.firstPass();
             } else {
