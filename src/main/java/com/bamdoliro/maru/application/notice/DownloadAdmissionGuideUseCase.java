e1f601480008d3d0a354a1f29a7e7e4495b2898e
hsm <hll2071@naver.com>
2025-10-22 09:38:01 +0900

    feat(#79) : 입학요강 다운로드 추가
    입학요강 다운로드 API 추가했습니다.

---
+++ src/main/java/com/bamdoliro/maru/application/notice/DownloadAdmissionGuideUseCase.java
@@ -0,0 +1,21 @@
+package com.bamdoliro.maru.application.notice;
+
+import com.bamdoliro.maru.infrastructure.s3.FileService;
+import com.bamdoliro.maru.infrastructure.s3.constants.FolderConstant;
+import com.bamdoliro.maru.infrastructure.s3.dto.response.UrlResponse;
+import com.bamdoliro.maru.shared.annotation.UseCase;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@UseCase
+public class DownloadAdmissionGuideUseCase {
+
+    private final FileService fileService;
+
+    private static final String ADMISSION_GUIDE_FILENAME = "2026학년도 해운대고등학교 입학전형요강.pdf";
+
+    public UrlResponse execute() {
+        String downloadUrl = fileService.getDownloadPresignedUrl(FolderConstant.ADMISSION_GUIDE, ADMISSION_GUIDE_FILENAME);
+        return new UrlResponse(null, downloadUrl);
+    }
+}
